fb1f887dcf16873eed0d3ceaf4c790f673440da1
Anirudh Deshpande <adeshpande@salud.unm.edu>
2020-03-29 14:14:26 -0600

    Add other projects

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,1 +1,9 @@
-
+class ProductList extends React.Component {
+    render() {
+        return (
+            <div class='ui unstackable items'>
+                Hello, friend! I am a basic React Component.
+            </div>
+        );
+    }
+}
