4f7470d57ca5e5a37c9f35554f085ff33266acd6
Anirudh Deshpande <anirudh.deshpande1@gmail.com>
2020-04-19 11:56:54 -0600

    Add product list component and use props in product component

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,9 +1,60 @@
 class ProductList extends React.Component {
     render() {
+        
+        const products = Seed.products.sort((a,b)=>{
+            return b.votes - a.votes;
+        });
+
+        const productComponents = products.map((product) => {return (
+            <Product
+                key={'product-' + product.id} 
+                id={product.id}
+                title={product.title}
+                description={product.description}
+                url={product.url}
+                votes={product.votes}
+                submitterAvatarUrl={product.submitterAvatarUrl}
+                productImageUrl={product.productImageUrl}
+            />
+        )});
+
         return (
-            <div class='ui unstackable items'>
-                Hello, friend! I am a basic React Component.
+            <div className='ui unstackable items'>
+                {productComponents}
             </div>
         );
     }
 }
+
+class Product extends React.Component {
+    render() {
+        return(
+            <div className='item'>
+                <div className='image'>
+                    <img src={this.props.productImageUrl} />
+                </div>
+                <div className='middle aligned content'>
+                    <div className='header'>
+                        <a>
+                            <i className='large caret up icon' />
+                        </a>
+                        {this.props.votes}
+                    </div>
+                    <div className='description'>
+                        <a href={this.props.url}>{this.props.title}</a>
+                        <p>{this.props.description}</p>
+                    </div>
+                    <div className='extra'>
+                        <span>Submitted by:</span>
+                        <img className='ui avatar image' src={this.props.submitterAvatarUrl}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <ProductList />,
+    document.getElementById('content')
+);
