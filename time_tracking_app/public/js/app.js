8e9df3fb5d83b0018d1da833fbe2a9f0fc416df2
Anirudh Deshpande <anirudh.deshpande1@gmail.com>
2020-07-04 16:25:28 -0600

    Add editable timer dashboard and editable time list components to time tracking app

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,1 +1,37 @@
+class TimersDashboard extends React.Component {
 
+    render() {
+        return (
+            <div className='ui three column centered grid'>
+                <div className='column'>
+                    <EditableTimerList />
+                    <ToggableTimerForm isOpen={true} />
+                </div>    
+            </div>
+        );
+    }
+
+}
+
+class EditableTimerList extends React.Component {
+    render() {
+        return(
+            <div id="timers">
+                <EditableTimer
+                    title='Learn React'
+                    project='World Domination'
+                    elapsed='8986300'
+                    runningSince={null}
+                    editFormOpen={false}
+                />
+                <EditableTimer
+                    title='Learn extreme ironing'
+                    project='World Domination'
+                    elapsed='3890985'
+                    runningSince={null}
+                    editFormOpen={true}
+                />
+            </div>
+        );
+    }
+}
