370799b0b6e4f16d7e1ea535885c33e5f4b50e76
Anirudh Deshpande <anirudh.deshpande1@gmail.com>
2023-04-18 22:39:55 -0600

    Add time tracking app

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,43 +1,32 @@
 class TimersDashboard extends React.Component {
+    
     state = {
         timers: [
             {
-                title: 'Practice squat',
-                project: 'Gym Chores',
+                title: 'Learn React',
+                project: 'Web Development',
                 id: uuid.v4(),
-                elapsed: 5456099,
+                elapsed: 8986300,
                 runningSince: Date.now()
             },
             {
-                title: 'Bake squash',
-                project: 'Kitchen Chores',
+                title: 'Water Plants',
+                project: 'Chores',
                 id: uuid.v4(),
-                elapsed: 1273998,
+                elapsed: 4548900,
                 runningSince: null
-            }
+            }   
         ]
-    };
-
-    handleCreateFormSubmit = (timer) => {
-        this.createTimer(timer);
-    };
-
-    createTimer = (timer) => {
-        const t = helpers.newTimer(timer);
-        this.setState({
-            timers: this.state.timers.concat(t)
-        });
+    
     };
 
     render() {
         return (
             <div className='ui three column centered grid'>
                 <div className='column'>
-                    <EditableTimerList timers={this.state.timers} />
-                    <ToggableTimerForm 
-                        onFormSubmit={this.handleCreateFormSubmit}
-                    />
-                </div>    
+                    <EditableTimerList timers={this.state.timers}/>
+                    <ToggleableTimerForm />
+                </div>
             </div>
         );
     }
@@ -47,16 +36,16 @@ class TimersDashboard extends React.Component {
 class EditableTimerList extends React.Component {
     render() {
 
-        const timers = this.props.timers.map((timer) => {
-            return (<EditableTimer
+        const timers = this.props.timers.map((timer) => (
+            <EditableTimer
                 key={timer.id}
                 id={timer.id}
                 title={timer.title}
                 project={timer.project}
                 elapsed={timer.elapsed}
                 runningSince={timer.runningSince}
-            />);
-        });
+            />
+        ));
 
         return(
             <div id="timers">
@@ -64,36 +53,47 @@ class EditableTimerList extends React.Component {
             </div>
         );
     }
+
 }
 
 class EditableTimer extends React.Component {
-
+    
     state = {
         editFormOpen: false
-    };
+    }
+
+    handleFormOpen = () => {
+        this.setState({editFormOpen: true});
+    }
 
     render() {
+
         if(this.state.editFormOpen) {
-            return (
+            return(
                 <TimerForm
                     id={this.props.id}
                     title={this.props.title}
                     project={this.props.project}
                 />
-            );
+            )
         } else {
-            return (
+            return(
                 <Timer
                     id={this.props.id}
                     title={this.props.title}
                     project={this.props.project}
                     elapsed={this.props.elapsed}
                     runningSince={this.props.runningSince}
+                    formOpen={this.handleFormOpen}
                 />
-            );
+            )
         }
+
+        
     }
+
 }
+
 
 class TimerForm extends React.Component {
 
@@ -103,133 +103,102 @@ class TimerForm extends React.Component {
     };
 
     handleTitleChange = (e) => {
-        this.setState({ title: e.target.value });
-    };
+        this.setState({title: e.target.value});
+    }
 
     handleProjectChange = (e) => {
-        this.setState({ project: e.target.value });
-    };
-
-    handleSubmit = () => {
-        this.props.onFormSubmit({
-            id: this.props.is,
-            title: this.props.title,
-            project: this.props.project
-        })
+        this.setState({project: e.target.value});
     }
 
     render() {
-        const submitText = this.props.id ? 'Update' : 'Create';
+        const submitText = this.props.title ? 'Update' : 'Create';
         return (
-            <div className='ui centered card'>
-                <div className='content'>
-                    <div className='ui form'>
-                        <div className='field'>
+            <div className="ui centered card">
+                <div className="content">
+                    <div className="ui form">
+                        <div className="field">
                             <label>Title</label>
-                            <input 
-                                type='text' 
-                                value={this.state.title}
-                                onChange={this.handleTitleChange} 
-                            />
+                            <input type="text" value={this.state.title}
+                            onChange={this.handleTitleChange}/>
                         </div>
-                        <div className='field'>
+                        <div className="field">
                             <label>Project</label>
-                            <input 
-                                type='text' 
-                                value={this.state.project} 
-                                onChange={this.handleProjectChange}
-                            />
+                            <input type="text" value={this.state.project}
+                            onChange={this.handleProjectChange}/>
                         </div>
-                        <div className='ui two bottom attached buttons'>
-                            <button 
-                                className='ui basic blue button'
-                                onClick={this.handleSubmit}
-                            >
+                        <div className="ui two bottom attached buttons">
+                            <button className="ui basic blue button">
                                 {submitText}
                             </button>
-                            <button 
-                                className='ui basic red button'
-                                onClick={this.props.onFormClose}
-                            >
+                            <button className="ui basic red button">
                                 Cancel
                             </button>
                         </div>
-                    </div>    
+                    </div>
                 </div>
             </div>
         );
     }
 }
 
-class ToggableTimerForm extends React.Component {
-
+class ToggleableTimerForm extends React.Component {
     state = {
         isOpen: false
-    };
+    }
 
     handleFormOpen = () => {
         this.setState({isOpen: true});
-    };
-
-    handleFormClose = () => {
-        this.setState({isOpen: false});
-    };
-
-    handleFormSubmit = (timer) => {
-        this.props.onFormSubmit(timer);
-        this.setState({isOpen: false});
     }
 
     render() {
+
         if(this.state.isOpen) {
-            return(
-                <TimerForm
-                    onFormSubmit={this.handleFormSubmit}
-                    onFormClose={this.handleFormClose}
-                />
-            );
+            return (<TimerForm />);
         } else {
-            return(
-                <div className='ui basic content center aligned segment'>
-                    <button className='ui basic button icon' onClick={this.handleFormOpen}> 
-                        <i className='plus icon'/>
+            return (
+                <div className="ui basic content center aligned segment">
+                    <button className="ui basic button icon" onClick={this.handleFormOpen}>
+                        <i className="plus icon" />
                     </button>
                 </div>
             );
         }
+
+        
     }
 }
 
 class Timer extends React.Component {
     render() {
-        const elapsedString = helpers.renderElapsedString(this.props.elapsed, this.props.runningSince);
-
-        return (
-            <div className='ui centered card'>
-                <div className='content'>
-                    <div className='header'>
+        const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+        return(
+            <div className="ui centered card">
+                <div className="content">
+                    <div className="header">
                         {this.props.title}
                     </div>
-                    <div className='meta'>
+                    <div className="meta">
                         {this.props.project}
                     </div>
-                    <div className='center aligned description'>
+                    <div className="center aligned description">
                         <h2>{elapsedString}</h2>
                     </div>
-                    <div className='extra content'>
-                        <span className='right floated edit icon'>
-                            <i className='edit icon' />
+                    <div className="extra content">
+                        <span className="right floated edit icon" onClick={this.props.formOpen}>
+                            <i className="edit icon"/>
                         </span>
-                        <span className='right floated trash icon'>
-                            <i className='trash icon' />
-                        </span>    
-                    </div>    
+                        <span className="right floated trash icon">
+                            <i className="trash icon"/>
+                        </span>
+                    </div>
                 </div>
-                <div className='ui bottom attached blue basic button'>
+                <div className="ui bottom attached blue basic button">
                     Start
-                </div>    
+                </div>
             </div>
+
         );
+
     }
 }
 
